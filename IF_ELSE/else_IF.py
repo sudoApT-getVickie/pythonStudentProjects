fd0031d588dc66266ef82f5beb4fad0ac0c76f2b
Bernard Victor Kirinya <123264661+sudoApT-getVickie@users.noreply.github.com>
2026-08-03 15:21:06 +0300

    in

---
+++ IF_ELSE/else_IF.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter Student MARKS"))
+if marks >= 90:
+  print(marks, "Excellent")
+elif marks >=80:
+  print(marks,"Very Good")
+elif marks >=60:
+  print
+else:
+  print(marks, "This is below Average")
+
+  
